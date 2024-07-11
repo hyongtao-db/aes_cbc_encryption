174517f6aedd9a70770bbe8c37af042ca363ee7e
Yongtao Huang <yongtaoh2022@gmail.com>
2024-07-11 08:44:12 +0000

    use pkcs7Padding

--- golang/main.go
+++ golang/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"errors"
 
 	//"crypto/rand"
 	"crypto/sha256"
@@ -10,9 +12,29 @@ import (
 
 	//"io"
 
-	"github.com/enceve/crypto/pad"
 	"golang.org/x/crypto/pbkdf2"
 )
+
+var errUnpaddingFailed = errors.New("decrypt failed, perhaps you are using different keys to encrypt and decrypt")
+
+// pkcs7Padding use PKCS7 to fill data blcok
+// https://tools.ietf.org/html/rfc5652#section-6.3
+func pkcs7Padding(text []byte, blockSize int) []byte {
+	padding := blockSize - len(text)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(text, padtext...)
+}
+
+// pkcs7Unpadding use PKCS7 to unpad data blcok
+func pkcs7Unpadding(text []byte) ([]byte, error) {
+	length := len(text)
+	unpadding := int(text[length-1])
+	var rest = length - unpadding
+	if rest < 0 {
+		return text, errUnpaddingFailed
+	}
+	return text[:rest], nil
+}
 
 func main() {
 
@@ -37,15 +59,17 @@ func main() {
 	ciphertext = make([]byte, (len(plaintext)/16+1)*aes.BlockSize)
 	salt := []byte{'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'}
 	fmt.Printf("salt: %v\n", salt)
-	pkcs7 := pad.NewPKCS7(aes.BlockSize)
-	pad1 := pkcs7.Pad(plaintext)
+	// pkcs7 := pad.NewPKCS7(aes.BlockSize)
+	// pad1 := pkcs7.Pad(plaintext)
+	pad1 := pkcs7Padding(plaintext, aes.BlockSize)
 	fmt.Printf("pad1: %v\n", pad1)
 
 	blk := cipher.NewCBCEncrypter(block, salt)
 	blk.CryptBlocks(ciphertext, pad1)
 	blk = cipher.NewCBCDecrypter(block, salt)
 	blk.CryptBlocks(plain, ciphertext)
-	plain, _ = pkcs7.Unpad(plain)
+	// plain, _ = pkcs7.Unpad(plain)
+	plain, _ = pkcs7Unpadding(plain)
 
 	fmt.Printf("Key size:\t%d bits\n", size*8)
 	fmt.Printf("Message:\t%s\n", msg)
